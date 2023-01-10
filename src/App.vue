88a9477f9d0c07c20d510b58fbf61b02f600413b
CodeBeans2022 <jamieleekinnear21@gmail.com>
2023-01-10 16:35:54 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="gridContainer">
+    <router-view/>
+    <Navbar/>
+</div>
 </template>
 
+<script>
+
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+.gridContainer{
+  display: grid;
+  grid-template-columns: 7fr 1fr;
+  grid-template-rows: 1fr;
+
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="nav-background">
+  <nav class="nav nav-tabs flex-column">
+     <router-link to="/"><img src="../assets/initails.png"></router-link>
+     <router-link class="nav-link nav-text-color" to="/">Home</router-link> 
+     <router-link class="nav-link nav-text-color" to="/about">About</router-link> 
+     <router-link class="nav-link nav-text-color" to="/resume">Resume</router-link> 
+     <router-link class="nav-link nav-text-color" to="/projects">Projects</router-link> 
+     <router-link class="nav-link nav-text-color" to="/testimonials">Testimonials</router-link> 
+     <router-link class="nav-link nav-text-color" to="/contact">Contact Me</router-link>
+  </nav>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<style>
+
+.nav-background{
+    background-color: rgba(0, 0, 0, 0.926);
+    display: flex;
+  justify-content: center;
+
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  overflow: hidden;
+  min-height: 100vh;
+}
+
+
+nav img{
+  height: 30px;
+  width: 60px;
+  top: 25px;
+  left: 40px;
+}
+
+nav {
+  width: fit-content;
+  height: 65vh;
+  border-radius: 20px;
+  margin-top: 100px;
+  justify-content: space-around;
+  text-decoration: none;
+  background-color: darkorchid;
+  padding: 0 12px !important;
+}
+
+nav .nav-text-color{
+  width: 100%;
+  border-bottom-right-radius: 5px;
+ border-bottom-left-radius: 5px;
+}
+
+nav .nav-text-color:hover{
+  background-color: orchid;
+  color: white;
+}
+
+nav .nav-link:visited{
+  color: white;
+}
+
+nav .nav-link:active{
+  background-color: orchid;
+}
+
+nav .nav-text-color{
+  color: white;
+}
+
+</style>
