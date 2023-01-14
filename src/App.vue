1d9d2bafc488368fe28f41b1dc9e30af28b00171
CodeBeans2022 <jamieleekinnear21@gmail.com>
2023-01-14 15:11:53 +0200

    update

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="gridContainer">
+    <router-view/>
+    <Navbar/>
+</div>
+</template>
+
+<script>
+
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+.gridContainer{
+  display: grid;
+grid-template-columns: 7fr 1fr;
+  grid-template-rows: 1fr;
+  min-height: 100vh;
+
+}
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="background">
+    <div class="homeText">
+    <h1 class="display-6">Bonjour, I'm Jamie-Lee Kinnear.</h1>
+    <h2 class="lead">Aspiring Full-Stack Developer and I.T Engineer.</h2>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+name: 'Home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.homeText{
+    width: 100% ;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.background{
+    width: 100%;
+    height: 100vh;
+    background-image: url(https://i.postimg.cc/5NvPPhgY/myimage.png);
+    background-size: cover;
+    background-repeat: no-repeat;
+    box-shadow: inset 120px 100px 250px #000000, inset -120px -100px 250px #000000;
+}
+
+h1, h2{
+    color: white;
+    text-align: center;
+}
+</style>
